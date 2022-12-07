a66f9f9d9b607d9f91818b0b95f0fd9c4e13a981
Amir Mehrabian <aut0run2011@gmail.com>
2022-12-07 22:35:17 +0330

    Update LicenseValidationController.php

--- src/Http/Controllers/Api/LicenseValidationController.php
+++ src/Http/Controllers/Api/LicenseValidationController.php
@@ -18,22 +18,28 @@ class LicenseValidationController extends BaseController
      */
     public function licenseValidate(Request $request, License $license)
     {
-        $_license = $license->select(
-            'domain',
-            'license_key',
-            'status',
-            'expiration_date',
-            'is_trial',
-            'is_lifetime',
-            'deleted_at',
-            'created_at',
-            'updated_at'
-        )->where('id', auth()->user()->id)->first();
+        $_license = License::where('id', auth()->user()->id)->first();
 
         $data = $request->input();
 
         Event::dispatch(new LicenseChecked($_license, $data));
 
         return $_license;
+    }
+
+    /**
+     * Validate given license
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function checkAllowedUsers(Request $request, License $license)
+    {
+        $_license = License::where('id', auth()->user()->id)->first();
+
+        $data = $request->input();
+
+        Event::dispatch(new LicenseChecked($_license, $data));
+
+        return $_license->allowed_users;
     }
 }
